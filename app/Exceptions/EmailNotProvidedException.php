c5882fe286b31380ffb2531419cbe874a95e8ce0
michaelbotes <michael@ourhood.co.za>
2017-03-22 10:22:32 +0200

    -added custom EmailNotProvidedException and tested in Test Controller

--- app/Exceptions/EmailNotProvidedException.php
+++ app/Exceptions/EmailNotProvidedException.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Exceptions;
+
 class EmailNotProvidedException extends \Exception
 {
     
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Widget;
+use App\Exceptions\EmailNotProvidedException;
 
 class TestController extends Controller
 {
@@ -14,7 +15,7 @@ class TestController extends Controller
      */
     public function index()
     {
-        $result = Widget::findOrFail(200);
+       throw new EmailNotProvidedException('facebook');
     }
 
     /**
